a47e8b1511126b087a074e851072b6e841da79a5
LukaJovanovic357 <jovanovicluka276@outlook.com>
2026-06-15 19:32:47 +0200

    add project model and relation to users

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Base(DeclarativeBase):
@@ -12,3 +12,18 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(100), unique=True, nullable=False)
     email = Column(String(255), unique=True, nullable=False)
+
+    # RELACIJA
+    projects = relationship("Project", back_populates="owner")
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(150), nullable=False)
+    description = Column(String(255), nullable=True)
+
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="projects")
--- app/schemas.py
+++ app/schemas.py
@@ -4,11 +4,24 @@ class UserCreate(BaseModel):
     username: str
     email: EmailStr
 
-
 class UserResponse(BaseModel):
     id: int
     username: str
     email: EmailStr
+
+    class Config:
+        from_attributes = True
+
+class ProjectCreate(BaseModel):
+    name: str
+    description: str | None = None
+    owner_id: int
+
+class ProjectResponse(BaseModel):
+    id: int
+    name: str
+    description: str | None
+    owner_id: int
 
     class Config:
         from_attributes = True
