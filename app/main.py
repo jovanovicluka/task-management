354fdc3013db6fb56da9d4449ce3ab96ee0f8c2e
LukaJovanovic357 <jovanovicluka276@outlook.com>
2026-06-15 19:40:33 +0200

    add route for projects and single project

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from app import models, schemas
 from app.database import engine, get_db
 from app.models import Base
+from app.models import Project
 
 app = FastAPI()
 
@@ -27,3 +28,35 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(db_user)
 
     return db_user
+
+
+@app.get("/projects", response_model=list[schemas.ProjectResponse])
+def get_projects(db: Session = Depends(get_db)):
+    return db.query(Project).all()
+
+
+@app.get("/projects/{project_id}", response_model=schemas.ProjectResponse)
+def get_project(project_id: int, db: Session = Depends(get_db)):
+
+    project = db.query(Project).filter(Project.id == project_id).first()
+
+    if not project:
+        return {"error": "Project not found"}
+
+    return project
+
+
+@app.post("/projects", response_model=schemas.ProjectResponse)
+def create_project(project: schemas.ProjectCreate, db: Session = Depends(get_db)):
+
+    db_project = Project(
+        name=project.name,
+        description=project.description,
+        owner_id=project.owner_id
+    )
+
+    db.add(db_project)
+    db.commit()
+    db.refresh(db_project)
+
+    return db_project
